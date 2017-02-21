2b67945cac0515ea953c154a03f4d780c802bbb7
gejiaheng <gejiaheng@gmail.com>
2017-02-21 21:20:24 +0800

    format

--- sample/src/main/java/com/sample/mdlicense/MainActivity.java
+++ sample/src/main/java/com/sample/mdlicense/MainActivity.java
@@ -1,8 +1,8 @@
 package com.sample.mdlicense;
 
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import com.ge.droid.mdlicense.Library;
